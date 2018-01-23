a3587465b8c775a175a498cb3d8e49175a76cdd4
DmitryVesennyi <35714492+DmitryVesennyi@users.noreply.github.com>
2018-01-23 10:36:51 +0700

    Update settings.py

--- polygon/settings.py
+++ polygon/settings.py
@@ -35,9 +35,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'vk.vot.bot@gmail.com'
+EMAIL_HOST_USER = '*************************'
 
-EMAIL_HOST_PASSWORD = 'VITksfJ47U0kXpmzbIT6'
+EMAIL_HOST_PASSWORD = '************************'
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
